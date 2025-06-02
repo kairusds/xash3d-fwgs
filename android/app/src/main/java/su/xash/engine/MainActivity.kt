8f01db62a9485329856b1b99b2e76f1f512bbb46
kairusds <32636532+kairusds@users.noreply.github.com>
2025-06-03 00:34:21 +0800

    Android storage changes
    - Add MANAGE_EXTERNAL_STORAGE
    - Add simple read/write storage permission checker
    - Allow game audio recording

--- android/app/src/main/java/su/xash/engine/MainActivity.kt
+++ android/app/src/main/java/su/xash/engine/MainActivity.kt
@@ -1,6 +1,15 @@
 package su.xash.engine
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.os.Build
+import android.os.Environment
+import android.net.Uri
+import android.provider.Settings
+
+import androidx.core.app.ActivityCompat
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
@@ -10,12 +19,15 @@ import androidx.navigation.ui.setupActionBarWithNavController
 import su.xash.engine.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val FILE_ACCESS_REQUEST = 1
+
     private lateinit var binding: ActivityMainBinding
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        checkPermissions()
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
@@ -32,4 +44,26 @@ class MainActivity : AppCompatActivity() {
     override fun onSupportNavigateUp(): Boolean {
         return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
+
+    private fun checkPermissions() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R && !Environment.isExternalStorageManager()) {
+            val uri = Uri.parse("package:" + packageName)
+            val intent = Intent(Settings.ACTION_MANAGE_APP_ALL_FILES_ACCESS_PERMISSION, uri)
+            startActivityForResult(intent, FILE_ACCESS_REQUEST)
+        } else if (Build.VERSION.SDK_INT < Build.VERSION_CODES.R && checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED && checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE),
+                FILE_ACCESS_REQUEST
+            )
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == FILE_ACCESS_REQUEST) {
+            checkPermissions()
+        }
+    }
+
 }
